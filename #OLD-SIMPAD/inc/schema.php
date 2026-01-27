6ba9161c824a4f217caab4f461750f5502c1a0b1
ditofy <didevto@gmail.com>
2026-01-27 09:27:00 +0700

    Update

--- #OLD-SIMPAD/inc/schema.php
+++
@@ -1,6 +0,0 @@
-<?php
-date_default_timezone_set('Asia/Jakarta');
-$jns_surat = array('01' => "SKP", '02' => "SPTPD", '03' => "STPD", '04' => "SSPD", '05' => "SKPDKB");
-$list_schema = array('01' => "air_tanah", '02' => "reklame", '03' => "restoran", '04' => "ppj", '05' => "parkir", '06' => "hiburan", '07' => "hotel", '08' => "galianc",'09' => "bphtb");
-$list_pbjt = array ('03' => "JASA MAKANAN DAN/ATAU MINUMAN", '04' => "JASA TENAGA LISTRIK", '05' => "JASA PARKIR", '06' => "JASA KESENIAN DAN HIBURAN", '07' => "JASA PERHOTELAN" );
-?>
--- #OLD-SIMPAD/modul/wp/pdf.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$filename=$_GET['file'];?>
-<embed src="<?php echo $filename;?>" width=”800px” height=”2100px” /><embed src=”<?php echo $filename;?>” width=”800px” height=”2100px” />
-
